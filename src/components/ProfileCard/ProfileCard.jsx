ce972fe3620c2ca7acad4755c456c5b7d91b1f7b
Belén Peluffo <belen.peluffo@dirmod.com>
2024-04-09 18:13:38 -0300

    feat (Home & ProfileCard): adds styling.

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,9 +1,15 @@
+import styles from "../../app.module.css";
+
 const ProfileCard = ({ photo, name, title }) => {
   return (
     <div>
-      <img src={photo} alt={`La cara de ${name}`} />
-      <div>{name}</div>
-      <div>{title}</div>
+      <img
+        src={photo}
+        alt={`La cara de ${name}`}
+        className={styles.profilePic}
+      />
+      <div className={styles.rotation}>{name}</div>
+      <div className={styles.rotation}>{title}</div>
     </div>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,24 @@
 import ProfileCard from "../components/ProfileCard/ProfileCard";
+import profilePic from '../assets/images/26170583_10213023271648707_1006969560875683411_o.jpg';
 
 const Home = () => {
-//   const { data } = useContext(AppContext);
+  //   const { data } = useContext(AppContext);
 
   return (
-    <div>
-      <div>This is HOME</div>
-      <div>
-        <ProfileCard name={"Belén Peluffo"} title={"CREATIVA"} />
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+        }}
+      >
+        <ProfileCard
+          name={"Belén Peluffo"}
+          title={"CREATIVA"}
+          photo={profilePic}
+        />
       </div>
     </div>
   );
