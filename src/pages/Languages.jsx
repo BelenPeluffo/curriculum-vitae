8faaef7b4637f2e10ee142827bcbcbdaa4c2032d
Belén Peluffo <belen.peluffo@dirmod.com>
2024-04-11 13:34:28 -0300

    feat (Languages): creates basic styling.

--- src/pages/Languages.jsx
+++ src/pages/Languages.jsx
@@ -1,10 +1,27 @@
 import LanguageItem from "../components/LanguageItem/LanguageItem";
+import styles from "../app.module.css";
 
 const Languages = ({ items }) => {
   return (
-    <div>
-      <div>Esta creativa maneja varios</div>
-      <div>[i.djo.mas]</div>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        border: "4px dashed var(--brand-orange)",
+        borderRadius: "8px",
+        marginLeft: "20%",
+        marginRight: "20%",
+        // borderColor: "var(--brand-orange)",
+      }}
+    >
+      <div
+        className={styles.rotatedText}
+        style={{ fontSize: "var(--small-size)" }}
+      >
+        Esta creativa maneja varios
+      </div>
+      <div className={styles.languageTitle}>[i.djo.mas]</div>
       <div>
         {items
           ? items.map((item, index) => (
