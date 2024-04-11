60069c81bf2f0ba15fa967799f62e5dd436e456f
Belén Peluffo <belen.peluffo@dirmod.com>
2024-04-11 17:31:07 -0300

    feat (MainDataContainer): adds functionality to decide position of container.

--- src/components/MainDataContainer/MainDataContainer.jsx
+++ src/components/MainDataContainer/MainDataContainer.jsx
@@ -1,24 +1,29 @@
 import styles from "../../app.module.css";
 import DiagonalLine from "./DiagonalLine";
 
-const MainDataContainer = ({ sectionTitle, items }) => {
+const MainDataContainer = ({ sectionTitle, items, alignment }) => {
   const lineLength = items.length * 65;
-  console.log("diagonalLength?", lineLength);
+  console.log("container position?", alignment);
   return (
-    <div>
+    <div
+      className="data-container"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: alignment ? alignment : "start",
+      }}
+    >
       <div className={styles.sectionTitle}>{sectionTitle.toUpperCase()}</div>
       {items ? (
         <div style={{ width: "60%" }}>
-          <DiagonalLine x1={0} y1={0} diagonalDistance={lineLength} />
+          <DiagonalLine x1={0} y1={0} diagonalDistance={lineLength} alignment={alignment} />
           <ul>
             {items
               ? items.map((item, index) => (
                   <li
                     key={index}
                     className={styles.itemContainer}
-                    // style={{ marginLeft: diagonalSpace*(index+1) }}
                   >
-                    {/* <div className={styles.itemContainer}> */}
                     <div>{item.date}</div>
                     <div className={styles.contactItemTextBold}>
                       {item.title}
@@ -26,7 +31,6 @@ const MainDataContainer = ({ sectionTitle, items }) => {
                     <div className={styles.itemDescription}>
                       {item.description}
                     </div>
-                    {/* </div> */}
                   </li>
                 ))
               : null}
