f3632be5e1ddd274530a3433ed4589fea3793c31
Belén Peluffo <belen.peluffo@dirmod.com>
2024-04-11 17:32:16 -0300

    feat (DiagonalLine): BREAKING CHANGE adds functionality to decide position of line.

--- src/components/MainDataContainer/DiagonalLine.jsx
+++ src/components/MainDataContainer/DiagonalLine.jsx
@@ -1,6 +1,14 @@
-const DiagonalLine = ({ x1, y1, diagonalDistance: length }) => {
+const DiagonalLine = ({ x1, y1, diagonalDistance: length, alignment }) => {
   return (
-    <svg width="100%" height="100%" style={{ position: "absolute", left: 24 }}>
+    <svg
+      width="100%"
+      height="100%"
+      style={{
+        position: "absolute",
+        left: alignment === "start" || !alignment ? 24 : 'calc(100%-24px)',
+        // right: alignment === "end" ? 100 : null,
+      }}
+    >
       <line y1={y1} y2={length} stroke="var(--brand-orange)" strokeWidth={4} />
     </svg>
   );
