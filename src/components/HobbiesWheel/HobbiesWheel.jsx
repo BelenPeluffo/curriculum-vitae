1b7610927fe46e0133132ac73ec58c88cf31cb0f
Belén Peluffo <belen.peluffo@dirmod.com>
2024-04-11 17:22:53 -0300

    feat (HobbiesWheel): adds styling.

--- src/components/HobbiesWheel/HobbiesWheel.jsx
+++ src/components/HobbiesWheel/HobbiesWheel.jsx
@@ -1,9 +1,19 @@
 import { PieChart } from "@mui/x-charts/PieChart";
 import BrandLogo from "../../assets/images/logo.svg";
+import { useDrawingArea } from "@mui/x-charts";
 
 const HobbiesWheel = ({ items }) => {
+  const { width } = useDrawingArea();
+  console.log('width?', width);
   return (
-    <div style={{ display: "flex", justifyContent: "center", width: "100%", alignItems: "center" }}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        width: "100%",
+        alignItems: "center",
+      }}
+    >
       <PieChart
         series={[
           { paddingAngle: 5, innerRadius: 160, outerRadius: 200, data: items },
@@ -16,7 +26,7 @@ const HobbiesWheel = ({ items }) => {
       ></PieChart>
       <img
         src={BrandLogo}
-        style={{ position: "relative", top: "50%", right: "50%" }}
+        style={{ position: "absolute", top: "252%", right: "46%" }}
       />
     </div>
   );
